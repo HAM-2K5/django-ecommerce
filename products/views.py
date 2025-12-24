1ee75030a498d5ce7ccb4d23bf6dcc25ebc9a973
Ham <hamplivingston1020@gmail.com>
2025-12-24 15:52:05 +0000

    added url section and dummy templates for products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    else:
+        category = None
+
+    return render(request, 'products/product_list.html', {
+        'categories': categories,
+        'products': products,
+        'category': category
+    })
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'products/product_detail.html', {
+        'product': product
+    })
+
